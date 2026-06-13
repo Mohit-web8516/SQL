8b29f42a591fb4e233f20058c00d0603a925c3fd
mohit-8516 <mohitshrivastav429@gmail.com>
2026-06-13 12:03:13 +0530

    CREATE TABLE

---
+++ TABLEQUERY.sql
@@ -0,0 +1,24 @@
+/*create table*/ 
+-- CREATE TABLE users(
+-- id 	INT AUTO_INCREMENT PRIMARY KEY,
+-- name VARCHAR(100) NOT NULL,
+-- email VARCHAR (100) UNIQUE NOT NULL,
+-- gender ENUM('Male','Femal','Other'),
+-- date_of_birth DATE,
+-- created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+-- );
+
+-- SELECT * FROM users;
+-- --------------------------------------
+-- renaming the table
+-- RENAME TABLE users to programmers;
+
+-- ---------------------------------------
+select * from programmers;
+
+
+
+-- --------------------------
+-- ALTER THE TABLE 
+
+
