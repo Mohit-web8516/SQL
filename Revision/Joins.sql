6386503c0dae704f93ca8ed6cc4ab2948a6a6004
mohit-8516 <mohitshrivastav429@gmail.com>
2026-06-21 16:11:47 +0530

    LEFT JOIN

--- Revision/Joins.sql
+++ Revision/Joins.sql
@@ -1,9 +1,11 @@
 -- A JOIN is used to combine rows from two or more tables based on a related column.
 
 
--- --1. INNER JOIN
+--1. INNER JOIN
 
 -- Returns only the matching records from both tables.
+
+-------------------------------------------
 
 SELECT
     Customers.customer_id,
@@ -12,3 +14,17 @@ SELECT
 FROM Customers
 INNER JOIN Orders
 ON Customers.customer_id = Orders.customer_id;
+
+------------------------------------------
+
+-- 2. LEFT JOIN
+
+-- Returns all records from the left table and matching records from the right table.
+
+SELECT
+    Customers.customer_name,
+    Orders.product
+FROM Customers
+LEFT JOIN Orders
+ON Customers.customer_id = Orders.customer_id;
+
